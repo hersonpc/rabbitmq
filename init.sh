a426005d320a0598fe782eb7ba579b70e6fd650a
hersonpc <hersonpc@gmail.com>
2019-07-12 13:12:03 -0300

    Registro de usuários

--- init.sh
+++ init.sh
@@ -5,10 +5,15 @@ then
 	echo "Create Rabbitmq user";
 	# Create Rabbitmq user
 	( sleep 5 ; \
+	rabbitmqctl add_user $RABBITMQ_ADMIN_USER $RABBITMQ_ADMIN_PASS 2>/dev/null ; \
+	rabbitmqctl set_user_tags $RABBITMQ_ADMIN_USER administrator ; \
+	rabbitmqctl set_permissions -p / $RABBITMQ_ADMIN_USER  ".*" ".*" ".*" ; \
+
 	rabbitmqctl add_user $RABBITMQ_USER $RABBITMQ_PASSWORD 2>/dev/null ; \
-	rabbitmqctl set_user_tags $RABBITMQ_USER none ; \
+	rabbitmqctl set_user_tags $RABBITMQ_USER guest ; \
 	rabbitmqctl set_permissions -p / $RABBITMQ_USER  ".*" ".*" ".*" ; \
-	echo "*** User '$RABBITMQ_USER' with password '$RABBITMQ_PASSWORD' completed. ***" ; \
+	echo "*** Admin user is '$RABBITMQ_ADMIN_USER'. ***" ; \
+	echo "*** Guest user is '$RABBITMQ_USER' with password '$RABBITMQ_PASSWORD'. ***" ; \
 	echo "*** Log in the WebUI at port 15672 (example: http://localhost:15672) ***") & \
 	rabbitmq-server
 fi
